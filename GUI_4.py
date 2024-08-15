4cb574e3bb70cf1b3c57dde28bbdb14ab2419ffe
owner <milota@mindgrip.com>
2024-08-14 17:22:36 -0700

    removed tracking of .pyc files and updated .gitignore

--- GUI_4.py
+++ GUI_4.py
@@ -5,6 +5,7 @@ from deepgram_ASR import DeepgramTranscriber  # Assume this is your transcriber 
 from command_buffer import Command_Buffer
 from back_end import Back_End
 
+
 class Window(Back_End):
     def __init__(self, title="GUI 4", transcriber=None, number_of_buttons=3):
         super().__init__(number_of_buttons=number_of_buttons)
@@ -19,28 +20,15 @@ class Window(Back_End):
         self.command_entry = scrolledtext.ScrolledText(self.root, width=50, height=4)
         self.command_entry.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
-        self.buttions = []
+        self.buttons = []
         # comput spacing for the buttons
         pad_incroment = 300 / number_of_buttons
         for i in range(number_of_buttons):
-            button = tk.Button(self.root, text=f"Button {i}", command=lambda i=i: self.button_clicked(i))
+            button = tk.Button(self.root, text=f"Button {i}", command=lambda j=i: self.button_clicked(j))
             button.grid(row=0, column=1, padx=(0, 10), pady=10 + i * pad_incroment, sticky="ne")
-            # set the color so it is consistant with the back end
+            # set the color, so it is consistent with the back end
             button.config(background=super().get_button_color(i))
-            self.buttions.append(button)
-
-
-        # Create Button 1
-        # self.button1 = tk.Button(self.root, text="Button 1", command=self.button1_clicked)
-        # self.button1.grid(row=0, column=1, padx=(0, 10), pady=10, sticky="ne")
-        # # set the color so it is consistant with the back end
-        # self.button1.config(background=super().get_button_color(1))
-        #
-        # # Create Button 2
-        # self.button2 = tk.Button(self.root, text="Button 2", command=self.button2_clicked)
-        # self.button2.grid(row=0, column=1, padx=(0, 10), pady=40, sticky="ne")
-        # # set the color so it is consistant with the back end
-        # self.button2.config(background=super().get_button_color(2))
+            self.buttons.append(button)
 
         # Create a frame for the bottom buttons
         button_frame = tk.Frame(self.root)
@@ -64,7 +52,7 @@ class Window(Back_End):
         self.root.grid_columnconfigure(0, weight=1)
         self.root.grid_columnconfigure(1, weight=0)
 
-        # when I tryed it without the delayed_action list it did now work
+        # when I trued it without the delayed_action list it did now work
         self.delayed_action = []
 
         # Bind F4 key events
@@ -77,23 +65,26 @@ class Window(Back_End):
         if transcriber is None:
             self.transcriber = DeepgramTranscriber()
         else:
-            self.transcriber = transcriber # use the one it was given
+            self.transcriber = transcriber  # use the one it was given
         self.transcriber.set_on_result(self.update_text)
         self.is_transcribing = False
 
         self.Command_Buffer = Command_Buffer()
 
         print("the GUI is up and running")
+
     def on_f4_press(self, event):
         if not self.is_transcribing:
             print("F4 pressed")
             self.toggle_transcription()
+
     def on_f4_release(self, event):
         print("F4 released")
         if self.is_transcribing:
             self.toggle_transcription()
         # self.transcriber.pause()
         # self.toggle_button.config(text="Start Transcription")
+
     def on_f1_press(self, event):
         print("F1 pressed")
         # pop a command off the command buffer
@@ -103,12 +94,14 @@ class Window(Back_End):
         # replace the text in the text box with the pending command
         self.command_entry.delete('1.0', tk.END)
         self.command_entry.insert(tk.END, pending_command)
+
     def on_f5_press(self, event):
         print("F5 pressed")
         pending_command = self.Command_Buffer.get_command()
         # TODO insert run action here
         self.Command_Buffer.clear_buffer()
         self.text_area.delete('1.0', tk.END)
+
     # process typing in the text box
     def update_text(self, text):
         # add the text to the command
@@ -120,6 +113,7 @@ class Window(Back_End):
         self.command_entry.insert(tk.END, pending_command)
         # self.text_area.insert(tk.END, text + "\n")
         # self.text_area.see(tk.END)  # Scroll to the end
+
     def on_undo(self):
         # pop a command off the command buffer
         self.Command_Buffer.undo()
@@ -128,6 +122,7 @@ class Window(Back_End):
         # replace the text in the text box with the pending command
         self.command_entry.delete('1.0', tk.END)
         self.command_entry.insert(tk.END, pending_command)
+
     def toggle_transcription(self):
         if self.is_transcribing:
             self.transcriber.pause()
@@ -137,6 +132,7 @@ class Window(Back_End):
             self.transcriber.unpause()
             self.toggle_ASR_button.config(text="Stop Transcription")
             self.is_transcribing = True
+
     # widget callbacks ------------------------------
 
     def take_action(self, command):
@@ -147,47 +143,36 @@ class Window(Back_End):
         print("todo list size ", len(self.delayed_action))
         for command in self.delayed_action:
             command()
-        self.delayed_action = [] # clear the todo list
+        self.delayed_action = []  # clear the todo list
+
     def run_callback(self):
         command = self.command_entry.get("1.0", "end-1c")
         print(f"Running command: {command}")
         self.take_action(command)
-    # def button1_clicked(self):
-    #     self.gestures += "button 1 was indicated"
-    #     print("Button 1 clicked")
-    # def button2_clicked(self):
-    #     self.gestures += "button 2 was indicated"
-    #     print("Button 2 clicked")
-    # agent tolls calls ------------------------------
 
     def button_clicked(self, button_index):
         description = f"Button {button_index} was indicated"
         self.gestures += description
         print(description)
+
     def get_button_color(self, button_index):
         print("Getting button color", button_index)
         memory_color = super().get_button_color(button_index)
-        #gui_color = self.buttions[button_index].cget("background")
-        # if button_index == 1:
-        #     gui_color = self.button1.cget("background")
-        # elif button_index == 2:
-        #     gui_color = self.button2.cget("background")
-        #assert memory_color == gui_color
         return memory_color
+
     def set_button_color(self, button_index, color):
         # Capture the class and instance in the lambda
         cls = super(Window, self)
         self.delayed_action.append(lambda: cls.set_button_color(button_index, color))
-        self.delayed_action.append(lambda : self.buttions[button_index].config(background=color))
-        # if button_index == 1:
-        #     self.delayed_action.append(lambda : self.button1.config(background=color))
-        # elif button_index == 2:
-        #     self.delayed_action.append(lambda : self.button2.config(background=color))
+        self.delayed_action.append(lambda: self.buttons[button_index].config(background=color))
+
     def set_run_callback(self, run_callback_function):
         print("Setting run callback function")
         self.run_callback_function = run_callback_function
+
     def run(self):
         self.root.mainloop()
+
 def main():
     # create the window
     transcriber = DeepgramTranscriber()
@@ -196,4 +181,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
